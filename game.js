b150084983b48bedb52d5bc9ba927a25e6aa4df9
Kostenko-Yaroslav <yaroslavkostenko34@gmail.com>
2024-07-29 17:23:08 +0300

    Load the sprites and display them

--- game.js
+++ game.js
@@ -1,12 +1,20 @@
 class mainScene {
-  preload() {}
-  create() {}
+  preload() {
+    this.load.image("player1", "./assets/player.png");
+    this.load.image("player2", "./assets/player.png");
+    this.load.image("ball", "./assets/ball.png");
+  }
+  create() {
+    this.player1 = this.add.sprite(100, 100, "player1");
+    this.player2 = this.add.sprite(100, 300, "player2");
+    this.ball = this.add.sprite(200, 100, "ball");
+  }
   update() {}
 }
 
 new Phaser.Game({
-  width: 700,
-  height: 500,
+  width: 670,
+  height: 580,
   backgroundColor: "#000",
   scene: mainScene,
   physics: { default: "arcade" },
