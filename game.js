dbabbd68b2a47147b8a8e2475fcc701d751ca8d6
Kostenko-Yaroslav <yaroslavkostenko34@gmail.com>
2024-07-29 17:40:05 +0300

    Set the basic physical parameters of the ball

--- game.js
+++ game.js
@@ -5,9 +5,13 @@ class mainScene {
     this.load.image("ball", "./assets/ball.png");
   }
   create() {
-    this.player1 = this.add.sprite(100, 100, "player1");
-    this.player2 = this.add.sprite(100, 300, "player2");
-    this.ball = this.add.sprite(200, 100, "ball");
+    this.player1 = this.add.sprite(100, 500, "player1");
+    this.player2 = this.add.sprite(560, 500, "player2");
+    this.ball = this.physics.add.sprite(335, 200, "ball");
+
+    this.ball.setVelocity(-150, 150);
+    this.ball.body.collideWorldBounds = true;
+    this.ball.body.bounce.set(1);
   }
   update() {}
 }
